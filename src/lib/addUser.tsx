710d6b561a6b501e83c0bb71b9e638d2691109c5
XuanShine <xuan.ng@hotmail.com>
2024-03-07 21:39:51 +0100

    correction de la bonne adresse

--- src/lib/addUser.tsx
+++ src/lib/addUser.tsx
@@ -2,7 +2,7 @@ import { error } from "console"
 
 export default async function addUser(newuser: any){
 
-    const res = await fetch("http://localhost:3000/api", {
+    const res = await fetch("http://localhost:3000/api/create", {
         method : 'POST',
         body: JSON.stringify(newuser),
         

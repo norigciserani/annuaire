342a7d372363610e3268f871668921899fbb95dd
XuanShine <xuan.ng@hotmail.com>
2024-03-11 01:18:45 +0100

    delete chambredispo

---
+++ src/app/api/delete/chambredispo/[date]/[room]/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+const prisma = new PrismaClient()
+
+export async function DELETE(req: Request, context: any) {
+    const { params } = context
+    const date = params.date
+    const datetime = new Date(date)
+    const room: number = +params.room
+    const chambreadelete = await prisma.chambredispo.findMany({
+        where: {
+            date: datetime,
+            numero: room
+        }
+    })
+    if (chambreadelete.length != 1){
+        return NextResponse.json({ "status": "not ok" })
+    }
+    const deleteChambredispo = await prisma.chambredispo.delete({
+        where: {id: chambreadelete[0].id}
+    })
+    return NextResponse.json({ "status": "ok" })
+}

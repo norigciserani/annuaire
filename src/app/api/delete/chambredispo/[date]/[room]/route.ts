cb52aa36d82544f10c5aa87a2b34444de68f7d8c
XuanShine <xuan.ng@hotmail.com>
2024-03-12 12:11:17 +0100

    Correction bug delete many rooms with same name

--- src/app/api/delete/chambredispo/[date]/[room]/route.ts
+++ src/app/api/delete/chambredispo/[date]/[room]/route.ts
@@ -8,17 +8,21 @@ export async function DELETE(req: Request, context: any) {
     const date = params.date
     const datetime = new Date(date)
     const room: string = params.room
-    const chambreadelete = await prisma.chambredispo.findMany({
+    const chambreadelete = await prisma.chambredispo.deleteMany({
         where: {
             date: datetime,
             numero: room
         }
     })
-    if (chambreadelete.length != 1){
-        return NextResponse.json({ "status": "not ok" })
-    }
-    const deleteChambredispo = await prisma.chambredispo.delete({
-        where: {id: chambreadelete[0].id}
-    })
+    // if (chambreadelete.length == 0){
+    //     return NextResponse.json({ "status": "not exist" })
+    // } else if (chambreadelete.length == 1){
+    //     const deleteChambredispo = await prisma.chambredispo.delete({
+    //         where: {id: chambreadelete[0].id}
+    //     })
+    //     return NextResponse.json({ "status": "ok" })
+    // } else {
+    //     const deleteChambredispo = await prisma.chambredispo.deleteMany(chambreadelete)
+    // }
     return NextResponse.json({ "status": "ok" })
 }

8acbe38d6ef3bbda45260a9bb637501f9a24d5ab
XuanShine <xuan.ng@hotmail.com>
2024-03-13 12:54:14 +0100

    Message error amélioré

--- src/lib/chambreDispo.tsx
+++ src/lib/chambreDispo.tsx
@@ -4,7 +4,7 @@ export default async function chambreDispo(date : string){
 
     const res = await fetch(`http://localhost:3000/api/chambredispo/${date}`,{cache: 'no-store'})
 
-    if (!res.ok) throw new Error('raté le fetch data')
+    if (!res.ok) throw new Error('Fetch /api/chambredispo/[date] failed in file /lib/chambreDispo.tsx')
     //faut utiliser le error boundaries dans la page si on mais pas try catch
 
     return res.json()

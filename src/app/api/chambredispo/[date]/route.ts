95f47937af050fcfe38a8e52c513e2c47f0a24d9
XuanShine <xuan.ng@hotmail.com>
2024-03-18 01:16:29 +0100

    bug date

--- src/app/api/chambredispo/[date]/route.ts
+++ src/app/api/chambredispo/[date]/route.ts
@@ -8,11 +8,17 @@ export async function GET(req:Request, context: any) {
     const { params } = context
     const date: string = params.date  // 2024-02-24
     const datetime: Date = new Date(date)
+    const datetime2: Date = new Date(date)
+    datetime2.setDate(datetime2.getDate() + 1)
     const chambres = await prisma.chambredispo.findMany({
         where: {
-            date: datetime
+            date: {
+                gte: datetime,
+                lt: datetime2
+            }
         }
     })
+    console.log(chambres)
     return NextResponse.json(chambres)
     
 }
